b9838be5ccc64f09e9c29fb9cdb510c1187db16e
Julian <julian_delisio@hotmail.com>
2022-03-03 00:40:36 -0300

    seteamos rutas y db

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 const express = require('express');
 
+const dotenv = require('dotenv');
+
+const cookieParser = require("cookie-parser");
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+//Seteamos el motor de plantillas
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.set('view engine', 'ejs');
+
+//Seteamos la carpeta public para archivos estaticos
+
+app.use(express.static('public'));
+
+//Seteamos node para manipular datos
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Seteamos carpeta para variables de entorno
+
+dotenv.config({ path: './env/.env' });
+
+//Para poder trabajar con las cookie
+
+//app.use(cookieParser);
+
+//Apuntamos la ruta raiz
+
+/* app.get('/', (req, res) => {
+    res.render('index');
+}); */
+
+//Llamar al router
+
+app.use("/", require("./routes/router"));
+
+//Levantamos el servidor en el puerto 3000
+
+app.listen(4000, () => {
+    console.log('Server is running on port 4000');
 });
